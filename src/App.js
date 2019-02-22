fbcf25a3a71f3181e2d586747ce56d5eb4c82de1
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-22 22:19:37 +0100

    Add route to App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import LoginForm from './components/LoginForm';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Hi, manager!
-        </header>
-        <LoginForm/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            Hi, manager!
+          </header>
+          <Route exact path='/' component={LoginForm}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
