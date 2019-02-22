7852ed498cff2d45d2cd4cee0fc2d13b7ae5d10b
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-22 22:14:32 +0100

    Add two cases to loginReducer - on email/pass change, expand initState, Add LoginForm as a child to App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
         <header className="App-header">
           Hi, manager!
         </header>
+        <LoginForm/>
       </div>
     );
   }
--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,7 +1,12 @@
 
 const initState = {
+  email: '',
+  password: '',
   isLoggedIn: false,
-  errorMessage: ''
+  errorMessage: '',
+  token: '',
+  inProgress: false,
+  redirectTo: null
 }
 
 const loginReducer = (state=initState, action) => {
@@ -9,7 +14,11 @@ const loginReducer = (state=initState, action) => {
     case 'USER_LOGGED_IN': 
       return {...state, isLoggedIn: true, errorMessage: action.errorMessage};
     case 'USER_LOGGED_OUT': 
-      return {...state, isLoggedIn: false, errorMessage: action.errorMessage}
+      return {...state, isLoggedIn: false, errorMessage: '', token: action.token};
+    case 'EMAIL_CHANGED':
+      return {...state, email: action.email};
+    case 'PASSWORD_CHANGED':
+      return {...state, password: action.password}
     default: 
       return state;
   }
