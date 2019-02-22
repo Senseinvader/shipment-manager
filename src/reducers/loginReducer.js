9040669947810f001420aeec724fda73d63de486
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-22 13:36:10 +0100

    Create loginReducer

---
+++ src/reducers/loginReducer.js
@@ -0,0 +1,16 @@
+
+const initState = {
+  user: null,
+  isLoggedIn: false
+}
+
+const loginReducer = (state=initState, action) => {
+  switch(action.type) {
+    case 'USER_LOGGED_IN': 
+      return {...state, isLoggedIn: true, user: action.user};
+    case 'USER_LOGGED_OUT': 
+      return {...state, isLoggedIn: false, user: null}
+    default: 
+      return state;
+  }
+}
