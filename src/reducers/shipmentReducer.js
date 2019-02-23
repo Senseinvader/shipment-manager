17064a40439c0726af22400909cd008b3aa3945f
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-23 21:20:51 +0100

    Add cases to shipmentReducer (create shipment, fetch shipments)

--- src/reducers/shipmentReducer.js
+++ src/reducers/shipmentReducer.js
@@ -1,10 +1,16 @@
 const initState = {
-
+  shipments: [],
 };
 
 const shipmentReducer = (state=initState, action) => {
   switch (action.type) {
+    case 'SHIPMENT_CREATED': 
+      return {...state, shipments: [...state.shipments, action.shipment]};
+    case 'SHIPMENTS_FETCHED': 
+      return {...state, shipments: action.shipments}
     default:
       return state;
   }
 }
+
+export default shipmentReducer;
