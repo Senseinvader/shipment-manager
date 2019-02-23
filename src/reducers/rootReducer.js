e73619b2da1f15459a87d9232e0885afd4075177
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-23 17:16:02 +0100

    Create SideBar component and redux infrastructure for it: shipmentReducer, shipmentActions, embed shipmentReducer into rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import loginReducer from './loginReducer';
+import shipmentReducer from './shipmentReducer';
 
 const rootReducer = combineReducers({
-  loginReducer
+  loginReducer,
+  shipmentReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/shipmentReducer.js
@@ -0,0 +1,10 @@
+const initState = {
+
+};
+
+const shipmentReducer = (state=initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
