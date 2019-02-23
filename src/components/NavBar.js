f8d89d02f0e5995f6a3202291d2b353458d7724b
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-23 16:34:17 +0100

    Create NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {NavLink} from 'react-router-dom'
+
+
+class NavBar extends Component {
+
+  render() {
+    const {onLogout, isLoggedIn} = this.props;
+    let logoutButton;
+
+    if(isLoggedIn) {
+      logoutButton = <button className='logout-button' onClick={onLogout}><NavLink to='/'>LOG OUT</NavLink></button>;
+    } else {
+      logoutButton = null;
+    }
+
+    return (
+      <nav className='nav-container'>
+        <div className='logo'>YSBM</div>
+        {logoutButton}
+      </nav>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.loginReducer.isLoggedIn,
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onLogout: () => { dispatch({type: 'USER_LOGGED_OUT'}) }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
