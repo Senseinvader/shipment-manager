fc6cd5e8b0b94e761f8922e6949829da3c02a801
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-24 22:03:22 +0100

    Add buttons to the footer of ItemComponent, style the component

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -6,7 +6,16 @@ class ItemContainer extends Component {
       <div className='items-container'>
         <div className="gap"> </div>
         <div className="items-list-container">
-          <h1>Let's get down to work!</h1>        
+          <h1>Let's get down to work!</h1>
+
+          <div className="items-buttons-container">
+            <button className="submit-button wide-button item-button" onClick={console.log('hi')}>
+              ADD ITEM
+            </button>
+            <button className="submit-button wide-button item-button" onClick={console.log('hi')}>
+              SEND SHIPMENT
+            </button>
+        </div>       
         </div>
       </div>
     )
