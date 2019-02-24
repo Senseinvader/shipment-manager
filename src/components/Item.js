75d8b09c4bca73832fc9850f6975e7b5785ecac6
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-24 22:37:01 +0100

    Create Item component

---
+++ src/components/Item.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+class Item extends Component {
+  render() {
+    const {code} = this.props;
+    return (
+      <div className='item-container'>
+        <div className="item-description">
+          {code}
+        </div>
+        <button className='submit-button'>DELETE</button>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Item);
