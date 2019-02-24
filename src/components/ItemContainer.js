858a8201adac2657606cd82ec7b4887f7084e051
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-24 22:38:06 +0100

    Add Items list markup with edge case messages to ItemContainer component

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Item from './Item';
 
 class ItemContainer extends Component {
+
+  renderList() {
+    const { currentShipment } = this.props;
+    if (!currentShipment) {
+      return (
+        <div className="message">
+          <h4>You don't have shipments yet. Please create first.</h4>
+        </div>
+      )
+    } else if (currentShipment && !currentShipment.items.length) {
+      return (
+        <div className="message">
+          <h4>This shipment doesn't have items. Please add first.</h4>
+        </div>
+      )
+    } else {
+      currentShipment.items.map(item => {
+        return (
+          <li className='shipment' key={item.id}><Item code={item.code}/></li>
+        ) 
+      })
+    }
+  }
+
   render() {
+    const { currentShipment } = this.props;
     return (
       <div className='items-container'>
         <div className="gap"> </div>
         <div className="items-list-container">
           <h1>Let's get down to work!</h1>
-
+          <ul className="flex-items-list">
+          {  }
+          </ul>
           <div className="items-buttons-container">
             <button className="submit-button wide-button item-button" onClick={console.log('hi')}>
               ADD ITEM
@@ -22,4 +51,16 @@ class ItemContainer extends Component {
   }
 }
 
-export default ItemContainer;
+const mapStateToProps = (state) => {
+  return {
+    currentShipment: state.shipmentReducer.currentShipment
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
