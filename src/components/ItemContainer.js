79fcc573daaa2350f90487782ecf7ecafb9bdb25
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-24 23:22:53 +0100

    Add conditional rendering to shipment items contents (if there are any)

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -4,38 +4,43 @@ import Item from './Item';
 
 class ItemContainer extends Component {
 
-  renderList() {
-    const { currentShipment } = this.props;
-    if (!currentShipment) {
-      return (
+  render() {
+    let list;
+    const { currentShipment, shipments } = this.props;
+
+    if (!shipments.length) {
+      list = (
         <div className="message">
           <h4>You don't have shipments yet. Please create first.</h4>
         </div>
       )
+    } else if (shipments.length && !currentShipment) {
+      list = (
+        <div className="message">
+          <h4>Please choose a shipment.</h4>
+        </div>
+      )
     } else if (currentShipment && !currentShipment.items.length) {
-      return (
+      list = (
         <div className="message">
           <h4>This shipment doesn't have items. Please add first.</h4>
         </div>
       )
     } else {
-      currentShipment.items.map(item => {
+      list = currentShipment.items.map(item => {
+        console.log(item)
         return (
           <li className='shipment' key={item.id}><Item code={item.code}/></li>
         ) 
       })
     }
-  }
 
-  render() {
-    const { currentShipment } = this.props;
     return (
       <div className='items-container'>
         <div className="gap"> </div>
         <div className="items-list-container">
-          <h1>Let's get down to work!</h1>
           <ul className="flex-items-list">
-          {  }
+          { list }
           </ul>
           <div className="items-buttons-container">
             <button className="submit-button wide-button item-button" onClick={console.log('hi')}>
@@ -53,7 +58,8 @@ class ItemContainer extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    currentShipment: state.shipmentReducer.currentShipment
+    currentShipment: state.shipmentReducer.currentShipment,
+    shipments: state.shipmentReducer.shipments
   }
 };
 
